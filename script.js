f79087934319191e27025c864d855778e9364bf2
Kert Tatoy <tatoy.kert.e@gmail.com>
2024-01-31 15:23:32 +0800

    my first javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+const btnIncrement = document.querySelector(".btn-increment");
+const btnDecrement = document.querySelector(".btn-decrement");
+const textValue = document.querySelector("h2");
+const btnChange = document.querySelector(".btn-change");
+
+let counter = 0;
+
+btnIncrement.addEventListener("click", function () {
+  counter++;
+  textValue.textContent = counter;
+});
+btnDecrement.addEventListener("click", function () {
+  if (counter > 0) {
+    counter--;
+    textValue.textContent = counter;
+  }
+});
+
+btnChange.addEventListener("click", function () {
+  textValue.style.backgroundColor = "red";
+});
